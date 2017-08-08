b4429811d5ce34024197afd58d9dadd5b4255177
Nohim Sandeepa <nohim.godage@gmail.com>
2017-08-08 22:41:03 +0530

    UserManagementService class methods connection to mysql db implemented and tested.

--- src/main/java/com/nohimys/service/UserManagementService.java
+++ src/main/java/com/nohimys/service/UserManagementService.java
@@ -7,32 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nohimys.dao.TrackeeInformationRepository;
+import com.nohimys.dao.TrackerInformationRepository;
 import com.nohimys.entity.TrackeeInformation;
+import com.nohimys.entity.TrackerInformation;
 import com.nohimys.model.TrackeeUser;
 
 @Service
 public class UserManagementService {
-	
+
 	@Autowired
 	private TrackeeInformationRepository trackeeInformationRepository;
 
+	@Autowired
+	private TrackerInformationRepository trackerInformationRepository;
+
 	public boolean validateLogin(String username, String password) {
-		if(username.equals("nohim") && password.equals("123")) {
+
+		TrackerInformation trackerInformation = trackerInformationRepository.findOne(username);
+
+		if (trackerInformation != null && trackerInformation.getLoginPassword() != null
+				&& trackerInformation.getLoginPassword().equals(password)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public Collection<TrackeeUser> getAllTrackeeUsers() {
-		
+
 		Iterable<TrackeeInformation> trackeeInformationIterator = trackeeInformationRepository.findAll();
-		
+
 		Collection<TrackeeUser> collection = new ArrayList<TrackeeUser>();
-		
-		for (TrackeeInformation trackeeInformation : trackeeInformationIterator) {
-			collection.add(new TrackeeUser(trackeeInformation.getUsername(),trackeeInformation.getFriendlyName()));	
+
+		if (trackeeInformationIterator != null) {
+			for (TrackeeInformation trackeeInformation : trackeeInformationIterator) {
+				collection.add(new TrackeeUser(trackeeInformation.getUsername(), trackeeInformation.getFriendlyName()));
+			}
 		}
-		
+
 		return collection;
 	}
 }
